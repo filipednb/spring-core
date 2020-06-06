d1dcac3f128d658754898c759f5cc4c04f801a61
Filipe Salgado Tagliacozzi <s2it_ftagliacozzi@pagseguro.com>
2020-06-05 22:29:10 -0300

    Seeing configuration via annotations

---
+++ src/main/java/spring/core/java/config/Artist.java
@@ -0,0 +1,20 @@
+package spring.core.java.config;
+
+public class Artist {
+
+    private String firstName;
+    private String lastName;
+
+    public Artist(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Artist{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/spring/core/java/config/ArtistConfiguration.java
@@ -0,0 +1,19 @@
+package spring.core.java.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ArtistConfiguration {
+
+    @Bean
+    public Artist artist() {
+        return new Artist("Filipe", "Tagliacozzi");
+    }
+
+    /*@Bean("AnotherArtist")
+    public Artist artist() {
+        return new Artist("Sebastian", "Doe");
+    }*/
+
+}
